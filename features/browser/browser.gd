c8fdf1d6b74d09d1c7448aa5a44fbe804d5577a9
Nick Cyran <nicyran@gmail.com>
2026-07-28 13:56:10 -0400

    updated browser.gd to be more useable

--- features/browser/browser.gd
+++ features/browser/browser.gd
@@ -1,6 +1,5 @@
 class_name Browser extends Control
 
-const BrowserTab = preload("res://features/browser/browser_tab.gd")
 const BrowserTabClusterScene := preload("res://features/browser/browser_tab_cluster.tscn")
 
 const MAX_TABS := 5
@@ -11,43 +10,31 @@ const ZOOM_MAX := 3.0
 const ZOOM_DEFAULT := 1.0
 
 @export var home_page: PackedScene
+@export var new_tab_btn: Button
+@export var zoom_out_btn: Button
+@export var zoom_reset_btn: Button
+@export var zoom_in_btn: Button
 
 @onready var _site_container: Control = $"Browser Content/Site"
 @onready var _tabs_hbox: HBoxContainer = $"Browser Content/TabStrip/TabsHBox"
 @onready var _back_btn: Button = $"Browser Content/NavBar/NavBarHBox/BackBtn"
 @onready var _forward_btn: Button = $"Browser Content/NavBar/NavBarHBox/ForwardBtn"
 @onready var _address_label: Label = $"Browser Content/NavBar/NavBarHBox/AddressBar/AddressLabel"
-@onready var _nav_hbox: HBoxContainer = $"Browser Content/NavBar/NavBarHBox"
 
 var _tabs: Array[BrowserTab] = []
 var _active_tab: int = -1
-var _new_tab_btn: Button
 var _page_tween: Tween
-var _zoom_level: float = 1.0
-var _zoom_label: Button
-var _zoom_out_btn: Button
-var _zoom_in_btn: Button
+var _zoom_level: float = ZOOM_DEFAULT
 
 
 func _ready() -> void:
 	_back_btn.pressed.connect(go_back)
 	_forward_btn.pressed.connect(go_forward)
-	_site_container.clip_contents = true
 	_site_container.resized.connect(_apply_zoom)
-
-	_build_zoom_controls()
-
-	_new_tab_btn = Button.new()
-	_new_tab_btn.text = "+"
-	_new_tab_btn.custom_minimum_size = Vector2(40, 0)
-	_new_tab_btn.size_flags_vertical = Control.SIZE_FILL
-	_new_tab_btn.theme_type_variation = &"NewTabButton"
-	_new_tab_btn.pressed.connect(_on_new_tab_pressed)
-	_tabs_hbox.add_child(_new_tab_btn)
-
-	for child in _site_container.get_children():
-		_site_container.remove_child(child)
-		child.queue_free()
+	new_tab_btn.pressed.connect(_on_new_tab_pressed)
+	zoom_out_btn.pressed.connect(_zoom_out)
+	zoom_reset_btn.pressed.connect(_zoom_reset)
+	zoom_in_btn.pressed.connect(_zoom_in)
 
 	open_new_tab(home_page)
 
@@ -145,10 +132,9 @@ func _show_page_animated(tab: BrowserTab) -> void:
 		page.modulate.a = 0.0
 		_site_container.add_child(page)
 
-		# Fade in the new page
 		if _page_tween:
 			_page_tween.kill()
-			
+
 		_page_tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 		_page_tween.tween_property(page, "modulate:a", 1.0, PAGE_FADE_DURATION)
 
@@ -169,17 +155,15 @@ func _active() -> BrowserTab:
 func _build_tab_ui(tab: BrowserTab) -> void:
 	var cluster: BrowserTabCluster = BrowserTabClusterScene.instantiate()
 	_tabs_hbox.add_child(cluster)
-	_tabs_hbox.move_child(cluster, _tabs_hbox.get_child_count() - 2)
+	_tabs_hbox.move_child(cluster, new_tab_btn.get_index())
 	cluster.tab_button.pressed.connect(_on_tab_clicked.bind(cluster))
 	cluster.close_button.pressed.connect(_on_tab_close_clicked.bind(cluster))
-	tab.button = cluster.tab_button
-	tab.close_btn = cluster.close_button
-	tab.container = cluster
+	tab.cluster = cluster
 
 
-func _find_tab_by_container(container: HBoxContainer) -> int:
+func _find_tab_by_cluster(cluster: BrowserTabCluster) -> int:
 	for i in range(_tabs.size()):
-		if _tabs[i].container == container:
+		if _tabs[i].cluster == cluster:
 			return i
 	return -1
 
@@ -187,20 +171,18 @@ func _find_tab_by_container(container: HBoxContainer) -> int:
 # --- UI Refresh ---
 
 func _refresh_tab_title(tab: BrowserTab) -> void:
-	if tab.button:
-		tab.button.text = tab.get_title()
+	tab.cluster.tab_button.text = tab.get_title()
 
 
 func _refresh_tab_visuals() -> void:
 	for i in range(_tabs.size()):
-		if _tabs[i].button:
-			_tabs[i].button.theme_type_variation = &"TabActive" if i == _active_tab else &"TabInactive"
+		_tabs[i].cluster.tab_button.theme_type_variation = &"TabActive" if i == _active_tab else &"TabInactive"
 
 
 func _refresh_nav_state() -> void:
 	var tab := _active()
-	_back_btn.disabled = not (tab and tab.can_go_back())
-	_forward_btn.disabled = not (tab and tab.can_go_forward())
+	_back_btn.disabled = !(tab && tab.can_go_back())
+	_forward_btn.disabled = !(tab && tab.can_go_forward())
 	_refresh_address_label()
 
 
@@ -209,26 +191,25 @@ func _refresh_address_label() -> void:
 	if !tab || !tab.current_page:
 		_address_label.text = ""
 		return
-		
+
 	var url := tab.get_url()
 	_address_label.text = url if url != "" else "about:blank"
 
 
 func _update_new_tab_visibility() -> void:
-	if _new_tab_btn:
-		_new_tab_btn.visible = _tabs.size() < MAX_TABS
+	new_tab_btn.visible = _tabs.size() < MAX_TABS
 
 
 # --- Signals ---
 
-func _on_tab_clicked(container: HBoxContainer) -> void:
-	var index := _find_tab_by_container(container)
+func _on_tab_clicked(cluster: BrowserTabCluster) -> void:
+	var index := _find_tab_by_cluster(cluster)
 	if index >= 0:
 		_switch_to_tab(index)
 
 
-func _on_tab_close_clicked(container: HBoxContainer) -> void:
-	var index := _find_tab_by_container(container)
+func _on_tab_close_clicked(cluster: BrowserTabCluster) -> void:
+	var index := _find_tab_by_cluster(cluster)
 	if index >= 0:
 		close_tab(index)
 
@@ -243,57 +224,25 @@ func _unhandled_input(event: InputEvent) -> void:
 	if !visible:
 		return
 
-	if event is InputEventKey and event.pressed:
-		if event.ctrl_pressed:
-			match event.keycode:
-				KEY_EQUAL:
-					_zoom_in()
-					get_viewport().set_input_as_handled()
-				KEY_MINUS:
-					_zoom_out()
-					get_viewport().set_input_as_handled()
-				KEY_0:
-					_set_zoom(ZOOM_DEFAULT)
-					get_viewport().set_input_as_handled()
+	if event is InputEventKey && event.pressed && event.ctrl_pressed:
+		match event.keycode:
+			KEY_EQUAL:
+				_zoom_in()
+				get_viewport().set_input_as_handled()
+			KEY_MINUS:
+				_zoom_out()
+				get_viewport().set_input_as_handled()
+			KEY_0:
+				_set_zoom(ZOOM_DEFAULT)
+				get_viewport().set_input_as_handled()
 
-	if event is InputEventMouseButton and event.pressed and event.ctrl_pressed:
+	if event is InputEventMouseButton && event.pressed && event.ctrl_pressed:
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
 			_zoom_in()
 			get_viewport().set_input_as_handled()
 		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			_zoom_out()
 			get_viewport().set_input_as_handled()
-
-
-func _build_zoom_controls() -> void:
-	_zoom_out_btn = Button.new()
-	_zoom_out_btn.text = "-"
-	_zoom_out_btn.custom_minimum_size = Vector2(28, 28)
-	_zoom_out_btn.size_flags_vertical = Control.SIZE_SHRINK_CENTER
-	_zoom_out_btn.theme_type_variation = &"NavButton"
-	_zoom_out_btn.tooltip_text = "Zoom out (Ctrl+-)"
-	_zoom_out_btn.pressed.connect(_zoom_out)
-
-	var reset_btn := Button.new()
-	reset_btn.custom_minimum_size = Vector2(48, 28)
-	reset_btn.size_flags_vertical = Control.SIZE_SHRINK_CENTER
-	reset_btn.theme_type_variation = &"NavButton"
-	reset_btn.text = "100%"
-	reset_btn.tooltip_text = "Reset zoom (Ctrl+0)"
-	reset_btn.pressed.connect(_zoom_reset)
-	_zoom_label = reset_btn
-
-	_zoom_in_btn = Button.new()
-	_zoom_in_btn.text = "+"
-	_zoom_in_btn.custom_minimum_size = Vector2(28, 28)
-	_zoom_in_btn.size_flags_vertical = Control.SIZE_SHRINK_CENTER
-	_zoom_in_btn.theme_type_variation = &"NavButton"
-	_zoom_in_btn.tooltip_text = "Zoom in (Ctrl+=)"
-	_zoom_in_btn.pressed.connect(_zoom_in)
-
-	_nav_hbox.add_child(_zoom_out_btn)
-	_nav_hbox.add_child(_zoom_label)
-	_nav_hbox.add_child(_zoom_in_btn)
 
 
 func _zoom_in() -> void:
@@ -310,9 +259,9 @@ func _zoom_reset() -> void:
 
 func _set_zoom(level: float) -> void:
 	_zoom_level = clampf(level, ZOOM_MIN, ZOOM_MAX)
-	_zoom_label.text = str(roundi(_zoom_level * 100)) + "%"
-	_zoom_out_btn.disabled = _zoom_level <= ZOOM_MIN
-	_zoom_in_btn.disabled = _zoom_level >= ZOOM_MAX
+	zoom_reset_btn.text = str(roundi(_zoom_level * 100)) + "%"
+	zoom_out_btn.disabled = _zoom_level <= ZOOM_MIN
+	zoom_in_btn.disabled = _zoom_level >= ZOOM_MAX
 	_apply_zoom()
 
 
--- features/browser/browser_tab.gd
+++ features/browser/browser_tab.gd
@@ -1,14 +1,11 @@
-extends RefCounted
+class_name BrowserTab extends RefCounted
 
 const MAX_BACK_HISTORY := 3
 
 var history: Array[PackedScene] = []
 var history_index: int = -1
 var current_page: Control = null
-
-var button: Button
-var close_btn: Button
-var container: HBoxContainer
+var cluster: BrowserTabCluster
 
 
 func navigate_to(scene: PackedScene) -> void:
@@ -48,12 +45,12 @@ func can_go_forward() -> bool:
 func get_current_scene() -> PackedScene:
 	if history_index >= 0 && history_index < history.size():
 		return history[history_index]
-		
+
 	return null
 
 
 func get_title() -> String:
-	if current_page && current_page is Site:
+	if current_page is Site:
 		var t := (current_page as Site).site_name
 		if t != "":
 			return t
@@ -61,7 +58,7 @@ func get_title() -> String:
 
 
 func get_url() -> String:
-	if current_page && current_page is Site:
+	if current_page is Site:
 		return (current_page as Site).site_url
 	return ""
 
@@ -70,7 +67,7 @@ func replace_page(scene: PackedScene) -> Control:
 	if current_page:
 		if current_page.get_parent():
 			current_page.get_parent().remove_child(current_page)
-			
+
 		current_page.queue_free()
 
 	current_page = scene.instantiate() as Control
@@ -90,8 +87,6 @@ func destroy() -> void:
 		current_page.queue_free()
 		current_page = null
 
-	if container:
-		container.queue_free()
-		container = null
-		button = null
-		close_btn = null
+	if cluster:
+		cluster.queue_free()
+		cluster = null
